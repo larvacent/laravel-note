2c152609ff1e076a087d8d19deede5c4b98c9cb4
xutongle <xutongle@gmail.com>
2020-03-21 16:14:38 +0800

    init

---
+++ src/NoteServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This is NOT a freeware, use is subject to license terms
+ * @copyright Copyright (c) 2010-2099 Jinan Larva Information Technology Co., Ltd.
+ * @link http://www.larva.com.cn/
+ * @license http://www.larva.com.cn/license/
+ */
+
+namespace Larva\Note;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class NoteServiceProvider
+ *
+ * @author Tongle Xu <xutongle@gmail.com>
+ */
+class NoteServiceProvider extends ServiceProvider
+{
+
+}
